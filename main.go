e95bcf1c8fbffcfea1017b9d2ce6441cfb61e335
Iogan <IoganVII@yandex.ru>
2022-10-05 07:22:32 +0300

    Охххх 1) Логика гоРутины - это тепреь воркер 2) Попытка сделать Всю синхронизацию через Каналы. Начао было хорошее, но потом пришлось добавить:  2.1) waiteGroup. Чтобы дождаться, пока воркеры закончат работать с каналом, и после закрываем канал.  2.2) Буферный канал с результатами. Чтобы обойти это, можно было написать отедьный воркер, который работал бы с каналом результатов - но это уже, как по мне, оверинженееринг.
     Вывод: на ГО можно очень по-разному писать, а как писать правильно?

--- main.go
+++ main.go
@@ -10,8 +10,6 @@ import (
 )
 
 func main() {
-
-	totalCount := 0
 	// Урлы ведут на Стенд Elma365, там АПИ модуля. Апи возвращает строку.
 	sliceUrl := []string{
 		"https://gtc5awmle2xwu.elma365.ru/api/extensions/3954a56f-8569-40c6-8f51-54ef45731c89/script/getDataString?id=4956bf29-3649-4f62-8587-1fac3a8d90cb",
@@ -55,28 +53,36 @@ func main() {
 		"https://gtc5awmle2xwu.elma365.ru/api/extensions/3954a56f-8569-40c6-8f51-54ef45731c89/script/getDataString?id=84367f05-50a6-4d86-bae5-d1a9fe7dbb67",
 		"https://gtc5awmle2xwu.elma365.ru/api/extensions/3954a56f-8569-40c6-8f51-54ef45731c89/script/getDataString?id=84367f05-50a6-4d86-bae5-d1a9fe7dbb67",
 	}
-	// Канал с буфером на 5 элементов. Чтобы ограничить создание гоРутин
-	chanel := make(chan int, 5)
-	// Группа ождиания, чтобы главная гоРутина ожидала выполнения остальных
+
+	// Канал, куда будут слоены урлы. Канал необходим воркеру для чтения
+	chanelWithUrl := make(chan string)
+	// Канал, куда будут складываться кол-во вхождений с каждой обработки url. Он будет буферизированный, чтоьы можно было складывать значения, а потом считать их.
+	// Сколькими разными способами можно эту задачи решить...иех
+	chanelResult := make(chan int, len(sliceUrl))
+
+	// Нужная, чтобы дожаться, пока Вворкеры закончат обрабатывать chanelWithUrl...Это очень грустно
 	wg := sync.WaitGroup{}
-	// Мьютекс для синхронизации работы с критической секцией
-	m := sync.Mutex{}
 
 	// Хочу посмотреть, за сколько выполняется логика программы
 	timeStart := time.Now()
 
+	// Запускаем воркеры - прогреть движки!
+	for i := 0; i < 5 && i <= len(sliceUrl); i++ {
+		go workerForSearchSubstringGo(i, chanelWithUrl, chanelResult, &wg)
+	}
+
 	// Начинаем перебирать слайс с URL
 	for _, url := range sliceUrl {
 		wg.Add(1)
-		// Занести данные в канал. Когда буфер будет заполнен - тут встанем на ожидание момента, когда в буфере освободится место
-		chanel <- 1
-		// Вызов фунции для получения данных по URL и подсчёта кол-ва вхождения подстроки
-		go searchSubstring(&totalCount, url, chanel, &wg, &m)
-		// Посмотрим - правда ли у нас тут 5 гоРутин работает
-		//fmt.Println("Кол-во гоРутин: ", len(chanel))
+		chanelWithUrl <- url
 	}
-
 	wg.Wait()
+	close(chanelWithUrl)
+
+	totalCount := 0
+	for i := len(chanelResult); i > 0; i-- {
+		totalCount += <-chanelResult
+	}
 
 	fmt.Println("Всего кол-во вхождений = ", totalCount)
 
@@ -84,35 +90,25 @@ func main() {
 }
 
 // Функция для подсчёта кол-во подстрок go в строке
-func searchSubstring(totalCount *int, url string, chanel chan int, wg *sync.WaitGroup, m *sync.Mutex) {
+func workerForSearchSubstringGo(id int, chanelWithUrl <-chan string, chanelResult chan<- int, wg *sync.WaitGroup) {
+	// Начинаем брать значение из канала, пока они там есть и канал открыт
+	for url := range chanelWithUrl {
 
-	// Вызываем функцию после окончания работы родительской функции
-	defer func() {
-		// Считываем данные из буфера
-		<-chanel
-		// Сигналим о том, что задача завершена
+		// Получаем Данные по URL
+		resp, er := http.Get(url)
+		if er != nil {
+			panic("Ошибка при вызове АПИ по url: " + url)
+		}
+		// Получаем байтМассив тела ответа
+		responseBody, er := io.ReadAll(resp.Body)
+		if er != nil {
+			panic("Ошибка при вызове АПИ по url: " + url)
+		}
+		// Получаем кол-во вхождений
+		countSubstring := strings.Count(string(responseBody), "go")
+		fmt.Println(id, ": Кол-во вхождуения для:", url, " = ", countSubstring)
+		// Записываем результат в канал для...Результатов
+		chanelResult <- countSubstring
 		wg.Done()
-	}()
-
-	// Получаем Данные по URL
-	resp, er := http.Get(url)
-	if er != nil {
-		panic("Ошибка при вызове АПИ по url: " + url)
 	}
-	// Получаем байтМассив тела ответа
-	responseBody, er := io.ReadAll(resp.Body)
-	if er != nil {
-		panic("Ошибка при вызове АПИ по url: " + url)
-	}
-	// Получаем кол-во вхождений
-	countSubstring := strings.Count(string(responseBody), "go")
-	fmt.Println("Кол-во вхождуения для:", url, " = ", countSubstring)
-
-	// Блокируем работу с критической секцией. всё-таки с одним элементом 5 потоков работают. Хочу синхронизацию
-	m.Lock()
-	*totalCount += countSubstring
-	// Пологировать можем, корректная ли работа с критической секцией
-	//fmt.Println(*totalCount)
-	m.Unlock()
-
 }
